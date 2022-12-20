3fc63d5c37ce4f0014a16cdca11610a62b71c7e1
shegy28 <shegy28@gmail.com>
2022-12-20 12:20:50 +0200

    Add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+ module.exports = {
+   entry: {
+     index: './src/index.js',
+   },
+   
+   devServer: {
+     static: './dist',
+   },
+
+   plugins: [
+     new HtmlWebpackPlugin({
+        template: './src/index.html',
+     }),
+   ],
+
+   output: {
+     filename: '[main].bundle.js',
+     path: path.resolve(__dirname, 'dist'),
+   },
+
+   module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+      ],
+   },
+
+   optimization: {
+    runtimeChunk: 'single',
+   },
+
+   mode: 'none'
+ };
